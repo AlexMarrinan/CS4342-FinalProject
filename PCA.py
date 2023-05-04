08431d845cf6bad2252314ace3a2f470011cd162
Beatrimo <59887924+Beatrimo@users.noreply.github.com>
2023-05-03 20:28:39 -0400

    Update PCA.py with PCA of only 2 sensors
    Update PCA.py with PCA of only 2 sensors

--- PCA.py
+++ PCA.py
@@ -21,19 +21,36 @@ sensor9 = reshaped_data[:,:,9]
 sensor10 = reshaped_data[:,:,10]
 sensor11 = reshaped_data[:,:,11]
 sensor12 = reshaped_data[:,:,12]
-trainingData = np.column_stack((sensor0,sensor1,sensor2,sensor3,sensor4,sensor5,sensor6,sensor7,sensor8,sensor9,sensor10,sensor11,sensor12))
+trainingDataAll = np.column_stack((sensor0,sensor1,sensor2,sensor3,sensor4,sensor5,sensor6,sensor7,sensor8,sensor9,sensor10,sensor11,sensor12))
 y = pd.read_csv("labels_CS.csv")
 y = y.values.reshape(-1)
 
 # Reduce dimensionality with PCA
-pca = PCA(n_components=2)
-pca_data = pca.fit_transform(trainingData)
+pcaAll = PCA(n_components=2)
+pca_dataAll = pcaAll.fit_transform(trainingDataAll)
 
-# Visualize data and labels in PCA space
-colors = ['blue' if label == 0 else 'red' for label in y]
+# Visualize data and labels in PCA space for all sensors
+colorsAll = ['blue' if label == 0 else 'red' for label in y]
 
-plt.scatter(pca_data[:, 0], pca_data[:, 1], c=colors)
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.scatter(pca_dataAll[:, 0], pca_dataAll[:, 1], c=colorsAll)
 plt.xlabel("PC1")
 plt.ylabel("PC2")
-plt.title("PCA visualization of training data")
+plt.title("PCA visualization of training data for all sensors")
+
+# Reduce dimensionality with PCA for sensors 2 and 4
+trainingDataReduced = np.column_stack((sensor2, sensor4))
+pcaReduced = PCA(n_components=2)
+pca_dataReduced = pcaReduced.fit_transform(trainingDataReduced)
+
+# Visualize data and labels in PCA space for sensors 2 and 4
+colorsReduced = ['blue' if label == 0 else 'red' for label in y]
+
+plt.subplot(1, 2, 2)
+plt.scatter(pca_dataReduced[:, 0], pca_dataReduced[:, 1], c=colorsReduced)
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.title("PCA visualization of training data with sensor 2 and sensor 4")
+plt.tight_layout()
 plt.show()
